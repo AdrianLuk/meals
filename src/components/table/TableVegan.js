09b2bb6b93fe1079c89e1c071c26edae27557962
Adrian <lukabaseball@hotmail.com>
2020-11-16 14:57:21 -0500

    changing all parseInts to parseFloat

--- src/components/table/TableVegan.js
+++ src/components/table/TableVegan.js
@@ -1,7 +1,7 @@
-import React, { Fragment, useContext } from "react";
-import "./table.scss";
-import FormContext from "../../contexts/Form";
-import { ADDON_PRICE } from "../constants";
+import React, { Fragment, useContext } from 'react';
+import './table.scss';
+import FormContext from '../../contexts/Form';
+import { ADDON_PRICE } from '../constants';
 // const isEmptyObject = object => {
 //     if (Object.entries(object).length === 0 && object.constructor === Object) {
 //         return true;
@@ -10,126 +10,111 @@ import { ADDON_PRICE } from "../constants";
 //     }
 // };
 const Table = ({
-    selectedPackage,
-    addOns,
-    vegans,
-    goal,
-    shipping,
-    selectedDelivery,
-    deliveryOption,
-    total,
+  selectedPackage,
+  addOns,
+  vegans,
+  goal,
+  shipping,
+  selectedDelivery,
+  deliveryOption,
+  total,
 }) => {
-    const form = useContext(FormContext);
-    return (
-        <table className="table unstriped">
-            <thead className="table__head">
-                <tr>
-                    <th>Order Summary</th>
-                    <th className="text-center">QTY</th>
-                    <th className="text-center">Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="table__row table__row--bold">
-                    <td>{selectedPackage.title.rendered}</td>
-                    <td />
-                    <td>{`$${parseFloat(selectedPackage.acf.price).toFixed(
-                        2
-                    )}`}</td>
-                </tr>
-                {goal && (
-                    <tr className="table__row table__row--indent-1">
-                        <td>{`${goal.acf.portion_description}`}</td>
-                        <td />
-                        <td>
-                            {parseInt(goal.acf.portion_price) > 0
-                                ? `$${parseFloat(
-                                      +goal.acf.portion_price *
-                                          +selectedPackage.acf.meal_count
-                                  ).toFixed(2)}`
-                                : `Included`}
-                        </td>
-                    </tr>
-                )}
-                {vegans?.map((vegan, index) => (
-                    <Fragment key={index}>
-                        <tr className="table__row table__row--bold">
-                            <td>{`${vegan.vegan.post_title}`}</td>
-                            <td className="text-center">{`x${vegan.count}`}</td>
-                            <td />
-                        </tr>
-                    </Fragment>
-                ))}
-                {form.comments.length > 0 && (
-                    <tr className="table__row">
-                        <td
-                            dangerouslySetInnerHTML={{
-                                __html: form.comments.replace(/\n/g, "<br>"),
-                            }}
-                        />
-                        <td />
-                        <td />
-                    </tr>
-                )}
-                {addOns.length > 0 && (
-                    <>
-                        <tr className="table__row table__row--bold">
-                            <td>Snacks</td>
-                            <td />
-                            <td>{`$${parseFloat(
-                                addOns.reduce(
-                                    (acc, curr) => acc + curr.count,
-                                    0
-                                ) * ADDON_PRICE
-                            ).toFixed(2)}`}</td>
-                        </tr>
-                        {addOns.map(addOn => (
-                            <tr
-                                key={addOn.snack.id}
-                                className="table__row table__row--indent-1">
-                                <td>{addOn.snack.post_title}</td>
-                                <td
-                                    className={`text-bold text-center`}>{`x${addOn.count}`}</td>
-                                <td />
-                            </tr>
-                        ))}
-                    </>
-                )}
-                {selectedDelivery !== "default" &&
-                    deliveryOption === "delivery" && (
-                        <Fragment>
-                            <tr className="table__row table__row--bold">
-                                <td>Delivery Location</td>
-                                <td />
-                                <td />
-                            </tr>
-                            <tr className="table__row table__row--indent-1">
-                                <td>
-                                    {
-                                        shipping.delivery_locations[
-                                            selectedDelivery
-                                        ].location
-                                    }
-                                </td>
-                                <td />
-                                <td>{`$${parseInt(
-                                    shipping.delivery_locations[
-                                        selectedDelivery
-                                    ].price
-                                ).toFixed(2)}`}</td>
-                            </tr>
-                        </Fragment>
-                    )}
-            </tbody>
-            <tfoot className="table__footer">
-                <tr>
-                    <th>Order Total</th>
-                    <th />
-                    <th className="text-center">{`$${total.toFixed(2)}`}</th>
-                </tr>
-            </tfoot>
-        </table>
-    );
+  const form = useContext(FormContext);
+  return (
+    <table className="table unstriped">
+      <thead className="table__head">
+        <tr>
+          <th>Order Summary</th>
+          <th className="text-center">QTY</th>
+          <th className="text-center">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="table__row table__row--bold">
+          <td>{selectedPackage.title.rendered}</td>
+          <td />
+          <td>{`$${parseFloat(selectedPackage.acf.price).toFixed(2)}`}</td>
+        </tr>
+        {goal && (
+          <tr className="table__row table__row--indent-1">
+            <td>{`${goal.acf.portion_description}`}</td>
+            <td />
+            <td>
+              {parseFloat(goal.acf.portion_price) > 0
+                ? `$${parseFloat(
+                    +goal.acf.portion_price * +selectedPackage.acf.meal_count
+                  ).toFixed(2)}`
+                : `Included`}
+            </td>
+          </tr>
+        )}
+        {vegans?.map((vegan, index) => (
+          <Fragment key={index}>
+            <tr className="table__row table__row--bold">
+              <td>{`${vegan.vegan.post_title}`}</td>
+              <td className="text-center">{`x${vegan.count}`}</td>
+              <td />
+            </tr>
+          </Fragment>
+        ))}
+        {form.comments.length > 0 && (
+          <tr className="table__row">
+            <td
+              dangerouslySetInnerHTML={{
+                __html: form.comments.replace(/\n/g, '<br>'),
+              }}
+            />
+            <td />
+            <td />
+          </tr>
+        )}
+        {addOns.length > 0 && (
+          <>
+            <tr className="table__row table__row--bold">
+              <td>Snacks</td>
+              <td />
+              <td>{`$${parseFloat(
+                addOns.reduce((acc, curr) => acc + curr.count, 0) * ADDON_PRICE
+              ).toFixed(2)}`}</td>
+            </tr>
+            {addOns.map((addOn) => (
+              <tr
+                key={addOn.snack.id}
+                className="table__row table__row--indent-1"
+              >
+                <td>{addOn.snack.post_title}</td>
+                <td className={`text-bold text-center`}>{`x${addOn.count}`}</td>
+                <td />
+              </tr>
+            ))}
+          </>
+        )}
+        {selectedDelivery !== 'default' && deliveryOption === 'delivery' && (
+          <Fragment>
+            <tr className="table__row table__row--bold">
+              <td>Delivery Location</td>
+              <td />
+              <td />
+            </tr>
+            <tr className="table__row table__row--indent-1">
+              <td>{shipping.delivery_locations[selectedDelivery].location}</td>
+              <td />
+              <td>{`$${parseFloat(
+                shipping.delivery_locations[selectedDelivery].price
+              ).toFixed(2)}`}</td>
+            </tr>
+          </Fragment>
+        )}
+      </tbody>
+      <tfoot className="table__footer">
+        <tr>
+          <th>Order Total</th>
+          <th />
+          <th className="text-center">{`$${total.toFixed(2)}`}</th>
+        </tr>
+      </tfoot>
+    </table>
+  );
 };
 
 export default Table;
--- src/components/vegan-steps/Choose.js
+++ src/components/vegan-steps/Choose.js
@@ -1,82 +1,81 @@
-import React, { Component } from "react";
-import CardItem from "../cards/CardItem_Title";
-import "./section.scss";
+import React, { Component } from 'react';
+import CardItem from '../cards/CardItem_Title';
+import './section.scss';
 // import axios from "axios";
 
 export class Choose extends Component {
-    state = {
-        packages: [],
-        // selectedPackage: null
-    };
+  state = {
+    packages: [],
+    // selectedPackage: null
+  };
 
-    render() {
-        // console.log(this.props);
-        const {
-            packages,
-            goals,
-            handleSelect,
-            selectedPackage,
-            selectedGoal,
-            handlePackageSelect,
-            // packageAmount
-        } = this.props;
-        const packageList = packages.map(packageItem => (
-            <CardItem
-                key={packageItem.id}
-                item={packageItem}
-                stateKey="selectedPackage"
-                selectedItem={selectedPackage}
-                handleSelect={handlePackageSelect}
-                handlePackageSelect={handlePackageSelect}
-                buttonText={`$${packageItem.acf.price}`}
-                description={`${packageItem.acf.meal_count} Personal Sized Meals`}
-                group="packages"
-                cardTitle={packageItem.title.rendered}
-            />
-        ));
-        const goalList = goals.map(goal => (
-            <CardItem
-                key={goal.id}
-                item={goal}
-                group="goals"
-                selectedItem={selectedGoal}
-                handleSelect={handleSelect}
-                stateKey="selectedGoal"
-                description={goal.acf.portion_description}
-                buttonText={
-                    parseInt(goal.acf.portion_price) > 0
-                        ? `$${parseInt(goal.acf.portion_price).toFixed(2)}`
-                        : `FREE`
-                }
-                cardTitle={goal.acf.portion_goal}
-            />
-        ));
-        return (
-            <section className="section section--choose">
-                <div className="section__item">
-                    <h2 className="section__heading">Select a Package</h2>
-                    <p className="section__subheading">
-                        Choose how many meals you intend to eat per day for six
-                        (6) days.
-                    </p>
-                    <div className="section__grid grid-x grid-margin-x align-large-justify align-spaced">
-                        {packageList}
-                    </div>
-                </div>
-                <hr />
-                <div className="section__item">
-                    <h2 className="section__heading">Select Your Goal</h2>
-                    <p className="section__subheading">
-                        We have 3 sizes for you to choose from: 4oz, 6oz, and
-                        8oz (+$1), to help you lose, maintain, and gain weight.
-                    </p>
-                    <div className="section__grid grid-x grid-margin-x align-large-justify align-spaced">
-                        {goalList}
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    // console.log(this.props);
+    const {
+      packages,
+      goals,
+      handleSelect,
+      selectedPackage,
+      selectedGoal,
+      handlePackageSelect,
+      // packageAmount
+    } = this.props;
+    const packageList = packages.map((packageItem) => (
+      <CardItem
+        key={packageItem.id}
+        item={packageItem}
+        stateKey="selectedPackage"
+        selectedItem={selectedPackage}
+        handleSelect={handlePackageSelect}
+        handlePackageSelect={handlePackageSelect}
+        buttonText={`$${packageItem.acf.price}`}
+        description={`${packageItem.acf.meal_count} Personal Sized Meals`}
+        group="packages"
+        cardTitle={packageItem.title.rendered}
+      />
+    ));
+    const goalList = goals.map((goal) => (
+      <CardItem
+        key={goal.id}
+        item={goal}
+        group="goals"
+        selectedItem={selectedGoal}
+        handleSelect={handleSelect}
+        stateKey="selectedGoal"
+        description={goal.acf.portion_description}
+        buttonText={
+          parseFloat(goal.acf.portion_price) > 0
+            ? `$${parseFloat(goal.acf.portion_price).toFixed(2)}`
+            : `FREE`
+        }
+        cardTitle={goal.acf.portion_goal}
+      />
+    ));
+    return (
+      <section className="section section--choose">
+        <div className="section__item">
+          <h2 className="section__heading">Select a Package</h2>
+          <p className="section__subheading">
+            Choose how many meals you intend to eat per day for six (6) days.
+          </p>
+          <div className="section__grid grid-x grid-margin-x align-large-justify align-spaced">
+            {packageList}
+          </div>
+        </div>
+        <hr />
+        <div className="section__item">
+          <h2 className="section__heading">Select Your Goal</h2>
+          <p className="section__subheading">
+            We have 3 sizes for you to choose from: 4oz, 6oz, and 8oz (+$1), to
+            help you lose, maintain, and gain weight.
+          </p>
+          <div className="section__grid grid-x grid-margin-x align-large-justify align-spaced">
+            {goalList}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Choose;
