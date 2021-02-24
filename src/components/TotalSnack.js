ab3d345109e12a702b0ce3b0079c868a48cc0c8c
Adrian <lukabaseball@hotmail.com>
2021-02-23 21:01:36 -0500

    copy edits

--- src/components/TotalSnack.js
+++ src/components/TotalSnack.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React from 'react';
 
 const TotalSnack = ({
-    step,
-    itemCount,
-    packagePrice,
-    selectedGoal,
-    selectedDelivery,
-    deliveryOption,
-    totalCustomizations,
-    customizationsRemaining,
-    total,
+  step,
+  itemCount,
+  packagePrice,
+  selectedGoal,
+  selectedDelivery,
+  deliveryOption,
+  totalCustomizations,
+  customizationsRemaining,
+  total,
 }) => {
-    return (
-        <div className="total">
-            <span className="total__label">{`${itemCount || 0} Meals`}</span>
-            {total && (
-                <span className="total__price">{`$${total.toFixed(2)}`}</span>
-            )}
-        </div>
-    );
+  return (
+    <div className="total">
+      <span className="total__label">{`${itemCount || 0} Snacks`}</span>
+      {total && <span className="total__price">{`$${total.toFixed(2)}`}</span>}
+    </div>
+  );
 };
 
 export default TotalSnack;
--- src/components/cards/CardListSnack.js
+++ src/components/cards/CardListSnack.js
@@ -1,41 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 // import SelectDropdown from "./SelectDropdown";
-import CardItemNumber from "./CardItem_Number";
-import SelectedPackageContext from "../../contexts/SelectedPackage";
-import FormContext from "../../contexts/Form";
+import CardItemNumber from './CardItem_Number';
+import SelectedPackageContext from '../../contexts/SelectedPackage';
+import FormContext from '../../contexts/Form';
 
 export const CardListSnack = ({
-    groups,
-    handleSelect,
-    hasNone,
-    stateKey,
-    selected,
-    selectedVariant,
-    setVariant,
-    groupName,
-    variantKey,
+  groups,
+  handleSelect,
+  hasNone,
+  stateKey,
+  selected,
+  selectedVariant,
+  setVariant,
+  groupName,
+  variantKey,
 }) => {
-    const maxSnackCount = useContext(SelectedPackageContext);
-    const form = useContext(FormContext);
-    const foodItemRow =
-        groups &&
-        groups.map(group => (
-            <CardItemNumber
-                itemsRemaining={form.snacksRemaining}
-                handleItemsChange={form.handleSnackChange}
-                key={group.id}
-                group={group}
-            />
-        ));
-    return (
-        <div className="section__item">
-            <h2 className="section__heading">{`Select your ${groupName}s`}</h2>
-            <p className="section__subheading">{`Select up to ${maxSnackCount?.acf?.size} snacks (required)`}</p>
-            <div className="section__grid grid-x grid-margin-x align-center">
-                {foodItemRow}
-            </div>
-        </div>
-    );
+  const maxSnackCount = useContext(SelectedPackageContext);
+  const form = useContext(FormContext);
+  const foodItemRow =
+    groups &&
+    groups.map((group) => (
+      <CardItemNumber
+        itemsRemaining={form.snacksRemaining}
+        handleItemsChange={form.handleSnackChange}
+        key={group.id}
+        group={group}
+      />
+    ));
+  return (
+    <div className="section__item">
+      <h2 className="section__heading">{`Select your ${groupName}s`}</h2>
+      <p className="section__subheading">{`Select ${maxSnackCount?.acf?.size} snacks`}</p>
+      <div className="section__grid grid-x grid-margin-x align-center">
+        {foodItemRow}
+      </div>
+    </div>
+  );
 };
 
 export default CardListSnack;
